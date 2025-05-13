60af6b46a4e22df7dc3c1761cfbb291c1fb72d17
Lawrence Dhliwayo <Lawrence.Dhliwayo@v2t.services>
2025-05-13 13:57:25 +0100

    Replace SSH with RM

--- arcgis_services/deploy/scripts/4_deploy_domains/add_domains.ps1
+++ arcgis_services/deploy/scripts/4_deploy_domains/add_domains.ps1
@@ -8,4 +8,11 @@ $scriptFile  = Join-Path -Path $scriptDir -ChildPath 'add_domains.py'
 
 Write-Output "Script to execute : $scriptFile"
 
-Start-Process python -ArgumentList $scriptFile -NoNewWindow 
+$proc = Start-Process python -ArgumentList $scriptFile -NoNewWindow -PassThru
+$handle = $proc.Handle # cache proc.Handle
+$proc.WaitForExit();
+
+if ($proc.ExitCode -ne 0) {
+    Write-Warning "$_ exited with status code $($proc.ExitCode)"
+    throw [System.Exception] "There was an error executing the python script"
+}
