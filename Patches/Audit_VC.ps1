8abdaca431344c783d465a02dd4e68a1e6440539
Lawrence Dhliwayo <Lawrence.Dhliwayo@v2t.services>
2025-08-01 12:16:15 +0100

    ..

--- Patches/Audit_VC.ps1
+++ Patches/Audit_VC.ps1
@@ -17,26 +17,48 @@ function Set-FileAuditRule {
         [System.Security.AccessControl.FileSystemAuditRule]$auditRule
     )
 
-$existingAcl = Get-Acl -Path $file
+    $existingAcl = Get-Acl -Path $file
     $existingAcl.AddAuditRule($auditRule) | Out-Null
     Set-Acl -Path $file -AclObject $existingAcl
 }
 
 $newAuditRule = Get-AuditRuleForFile
 
-# Visual Studio Redistributable for 2005 (VC++ 8.0) and 2008 (VC++ 9.0)
-Get-ChildItem "$ENV:SystemRoot\WinSxS\Fusion" -filter '*.dll' -ErrorAction SilentlyContinue -Recurse |
-Where-Object FullName -IMatch 'microsoft\.vc[89]0' |
+# Visual Studio C++ Redistributable DLLs from WinSxS (all versions)
+Get-ChildItem "$ENV:SystemRoot\WinSxS" -filter '*.dll' -ErrorAction SilentlyContinue -Recurse |
+Where-Object FullName -IMatch 'microsoft\.vc[0-9]+' |
 ForEach-Object {
     Set-FileAuditRule $_.FullName $newAuditRule
 }
 
-# Visual Studio Redistributable for 2010 (VC++ 10.0), 2012 (VC++ 11.0) and 2013 (VC++ 12.0)
+# Visual Studio C++ Redistributable DLLs from System32 and SysWOW64 (all versions)
+# Updated regex to match all VC++ versions (80, 90, 100, 110, 120, 140, 150, 160, etc.)
 $languageCodes = 'chs|cht|deu|enu|esn|fra|ita|jpn|kor|rus'
-$versions = '(1[012]0)'
+$versions = '([0-9]+0)'  # Matches any version ending in 0 (80, 90, 100, 110, etc.)
 $regex = "^((atl|msvc[pr]|vcamp|vccorlib|vcomp)$versions|mfc$versions(u|$languageCodes)?|mfcm$versions(u)?)\.dll$"
 Get-ChildItem "$ENV:SystemRoot\SysWOW64","$ENV:SystemRoot\System32" -filter '*.dll' |
 Where-Object Name -imatch $regex |
 ForEach-Object {
     Set-FileAuditRule $_.FullName $newAuditRule
+}
+
+# Additional search for newer VC++ redistributables that might use different naming patterns
+# This covers VC++ 2015, 2017, 2019, 2022 and beyond
+$modernVcPatterns = @(
+    'vcruntime\d+\.dll',
+    'msvcp\d+\.dll',
+    'vccorlib\d+\.dll',
+    'vcomp\d+\.dll',
+    'atl\d+\.dll',
+    'mfc\d+\.dll',
+    'mfcm\d+\.dll',
+    'mfcs\d+\.dll'
+)
+
+foreach ($pattern in $modernVcPatterns) {
+    Get-ChildItem "$ENV:SystemRoot\SysWOW64","$ENV:SystemRoot\System32" -filter '*.dll' |
+    Where-Object Name -imatch "^$pattern$" |
+    ForEach-Object {
+        Set-FileAuditRule $_.FullName $newAuditRule
+    }
 }
