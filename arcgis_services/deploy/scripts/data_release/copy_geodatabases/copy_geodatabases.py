8c2277fde166fb9e053f887e0f892c0cadb57a96
Lawrence Dhliwayo <Lawrence.Dhliwayo@v2t.services>
2025-09-03 14:44:03 +0100

    Copy geodb

--- arcgis_services/deploy/scripts/data_release/copy_geodatabases/copy_geodatabases.py
+++ arcgis_services/deploy/scripts/data_release/copy_geodatabases/copy_geodatabases.py
@@ -2,8 +2,6 @@
 
 # Copies all the geodatabases from the source to target as specified in the config file, if the update_in_release is set to True
 
-print("Start copy_geodatabases.py")
-print("Loading imports...")
 # Imports 
 
 import arcpy 
@@ -12,7 +10,7 @@ import logging
 import logging.handlers
 import os 
 import sys
-import arcpy
+
 
 
 class CopyGeodatabases:
@@ -25,19 +23,22 @@ class CopyGeodatabases:
         script_file = os.path.basename(__file__)
         formatter = logging.Formatter('%(asctime)s - {} - %(levelname)s - %(message)s'.format(script_file))
 
-        consoleHandler = logging.StreamHandler(sys.stdout)
-        consoleHandler.setLevel(logging.DEBUG)
-        consoleHandler.setFormatter(formatter)
+        # Configure handlers once to avoid duplicated logs if re-instantiated
+        if not self.__logger.handlers:
+            consoleHandler = logging.StreamHandler(sys.stdout)
+            consoleHandler.setLevel(logging.DEBUG)
+            consoleHandler.setFormatter(formatter)
 
-        fileHandler = logging.handlers.RotatingFileHandler(filename="copy_geodatabases.log",maxBytes=1024000, backupCount=10, mode="a")
-        fileHandler.setLevel(logging.INFO)
-        fileHandler.setFormatter(formatter)
+            log_dir = os.path.dirname(os.path.realpath(__file__))
+            log_path = os.path.join(log_dir, "copy_geodatabases.log")
+            fileHandler = logging.handlers.RotatingFileHandler(filename=log_path, maxBytes=1024000, backupCount=10, mode="a")
+            fileHandler.setLevel(logging.INFO)
+            fileHandler.setFormatter(formatter)
 
-        self.__logger.addHandler(consoleHandler)
-        self.__logger.addHandler(fileHandler)
+            self.__logger.addHandler(consoleHandler)
+            self.__logger.addHandler(fileHandler)
 
         self.__logger.info("Start")
-        self.__logger.info(arcpy.GetMessages())
 
         config = self.read_config_ini()
 
@@ -47,34 +48,39 @@ class CopyGeodatabases:
         
             source_gdb = config[config_key_arg]["source_gdb"]
             target_gdb = config[config_key_arg]["target_gdb"]
-            update_in_release = config[config_key_arg]["update_in_release"]
+            try:
+                update_in_release = config[config_key_arg]["update_in_release"]
+            except KeyError:
+                raise KeyError(f"Missing 'update_in_release' for section '{config_key_arg}' in config.ini")
 
-            should_update_gdb = self.set_should_upate_value(update_in_release)
+            should_update_gdb = self.set_should_update_value(update_in_release)
 
             if should_update_gdb:
-                self.__logger.info("{0}...copying {1} to {2}".format(config_key_arg,source_gdb, target_gdb))
+                self.__logger.info("{0}...copying {1} to {2}".format(config_key_arg, source_gdb, target_gdb))
                 self.execute_copy_gdb(source_gdb, target_gdb)
-                self.__logger.info("    ... finishined copying {0}".format(target_gdb))
+                self.__logger.info("    ... finished copying {0}".format(target_gdb))
             else:
                  self.__logger.info(config_key_arg + " - Data update for key is set to false and will be ignored") 
 
 
         self.__logger.info("End")
 
-    def set_should_upate_value(self, update_in_release):
-        if update_in_release.lower() == "true":
+    def set_should_update_value(self, update_in_release):
+        if isinstance(update_in_release, bool):
+            return update_in_release
+        value = str(update_in_release).strip().lower()
+        if value in ("true", "1", "yes", "on"):            
             return True
-        elif update_in_release.lower() == "false":
+        if value in ("false", "0", "no", "off"):
             return False
-        else: 
-            raise ValueError("update_in_release parameter is neither True or False")
+        raise ValueError("update_in_release parameter is neither True nor False")
 
 
     # Functions 
 
     def read_config_ini(self):
         path = os.path.dirname(os.path.realpath(__file__))
-        config_ini_path = os.path.join(path, "config\config.ini")
+        config_ini_path = os.path.join(path, "config", "config.ini")
         config = configparser.ConfigParser()
         # config.optionxform = str  # Case sensitive config values
         config.read(config_ini_path)
@@ -88,11 +94,28 @@ class CopyGeodatabases:
         try:
             arcpy.env.overwriteOutput = True
 
+            # Validate inputs
+            if not source_gdb:
+                raise ValueError("source_gdb is empty")
+            if not target_gdb:
+                raise ValueError("target_gdb is empty")
+
+            # Ensure destination directory exists
+            target_parent_dir = os.path.dirname(target_gdb)
+            if target_parent_dir and not os.path.exists(target_parent_dir):
+                os.makedirs(target_parent_dir, exist_ok=True)
+
+            # If target exists, delete first to avoid copy failures
+            if arcpy.Exists(target_gdb):
+                self.__logger.info("Target exists. Deleting: {0}".format(target_gdb))
+                arcpy.management.Delete(target_gdb)
+
+            self.__logger.info("Copying geodatabase using arcpy.Copy_management")
             arcpy.Copy_management(source_gdb, target_gdb)
 
         except Exception as e:
-            self.__logger.error(e)
-            raise Exception
+            self.__logger.exception("Failed to copy geodatabase from '{0}' to '{1}'".format(source_gdb, target_gdb))
+            raise
 
 if __name__ == "__main__":
     processor = CopyGeodatabases()
